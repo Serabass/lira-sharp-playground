0163632b548ba995de2440d5a79e40f8e00f23fb
Serabass <serabass565@gmail.com>
2024-02-22 05:03:07 +0600

    upd

--- build.ps1
+++ build.ps1
@@ -1,2 +1,10 @@
-docker build -t 192.168.31.21:5000/lira-sharp/playground .
-docker push 192.168.31.21:5000/lira-sharp/playground
+$REPO = "192.168.31.21:5000/lira-sharp/playground"
+
+podman build `
+  --tag $REPO `
+  --cache-from $REPO `
+  --cache-from $REPO/cache `
+  --cache-to   $REPO/cache `
+  .
+
+podman push $REPO
